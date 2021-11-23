93a34cf61bcf31aae4967e234b4508ad1268d870
YousukeSasaki <yosaprog@gmail.com>
2021-11-23 20:43:17 +0900

    Headerコンポーネントちゃんと分ける

--- frontend/components/RequestForm.vue
+++ frontend/components/RequestForm.vue
@@ -1,48 +1,52 @@
 <template>
-  <div :class="$style.container">
-    <v-form ref="form" v-model="valid" lazy-validation>
-      <v-text-field
-        v-model="question"
-        :counter="30"
-        :rules="questionRules"
-        label="問題"
-        placeholder="（例）タイピングに自信あり！"
-        required
-      ></v-text-field>
+  <v-row class="text-center">
+    <v-col class="mb-4">
+      <div :class="$style.container">
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            v-model="question"
+            :counter="30"
+            :rules="questionRules"
+            label="問題"
+            hint="（例）タイピングに自信あり！"
+            required
+          ></v-text-field>
 
-      <v-text-field
-        v-model="kana"
-        :counter="60"
-        :rules="kanaRules"
-        label="ひらがな"
-        placeholder="（例）たいぴんぐにじしんあり！"
-        required
-      ></v-text-field>
+          <v-text-field
+            v-model="kana"
+            :counter="60"
+            :rules="kanaRules"
+            label="ひらがな"
+            placeholder="（例）たいぴんぐにじしんあり！"
+            required
+          ></v-text-field>
 
-      <v-select
-        v-model="select"
-        :items="items"
-        item-text="ja"
-        item-value="en"
-        :rules="selectRules"
-        label="ジャンル"
-        required
-      ></v-select>
+          <v-select
+            v-model="select"
+            :items="items"
+            item-text="ja"
+            item-value="en"
+            :rules="selectRules"
+            label="ジャンル"
+            required
+          ></v-select>
 
-      <v-btn
-        :disabled="!valid"
-        color="success"
-        class="mr-4"
-        @click="submitRequest"
-      >
-        送信
-      </v-btn>
+          <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="submitRequest"
+          >
+            送信
+          </v-btn>
 
-      <v-btn color="error" class="mr-4" @click="reset">
-        クリア
-      </v-btn>
-    </v-form>
-  </div>
+          <v-btn color="error" class="mr-4" @click="reset">
+            クリア
+          </v-btn>
+        </v-form>
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -80,16 +84,16 @@ export default {
           createdAt: firebase.firestore.FieldValue.serverTimestamp(),
           updatedAt: firebase.firestore.FieldValue.serverTimestamp()
         }
-        db.collection('requests')
-          .doc('genre')
-          .collection(this.select)
-          .add(requestData)
-          .then(() => {
-            this.$refs.form.reset()
-          })
-          .catch((err) => {
-            console.log('error', err)
-          })
+        // db.collection('requests')
+        //   .doc('genre')
+        //   .collection(this.select)
+        //   .add(requestData)
+        //   .then(() => {
+        //     this.$refs.form.reset()
+        //   })
+        //   .catch((err) => {
+        //     console.log('error', err)
+        //   })
       }
     },
     reset() {
---
+++ frontend/components/common/Header.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-app-bar app color="primary" height="64">
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+
+      <v-img
+        alt="Vuetify Name"
+        class="shrink mt-1 hidden-sm-and-down"
+        contain
+        min-width="100"
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+        width="100"
+      />
+      <v-spacer></v-spacer>
+      <v-tabs background-color="primary" height="64" dark grow>
+        <v-tab nuxt to="/" class="white--text">Home</v-tab>
+        <v-tab nuxt to="/play" class="white--text">Play</v-tab>
+        <v-tab nuxt to="/request" class="white--text">Request</v-tab>
+        <v-tab nuxt to="/table" class="white--text">Table</v-tab>
+        <!-- <v-tab>
+          <NuxtLink class="white--text font-small" to="/admin/requests"
+            >admin
+          </NuxtLink>
+        </v-tab> -->
+      </v-tabs>
+    </div>
+    <div class="d-flex align-center ml-auto">
+      <template v-if="this.$auth.loggedIn">
+        <v-img
+          :src="this.$auth.$state.user.picture"
+          width="56"
+        />
+        <p class="mb-0 ml-3 white--text">{{ this.$auth.$state.user.name }}</p>
+      </template>
+      <v-btn
+        v-if="!this.$auth.loggedIn"
+        type="primary"
+        @click="loginWithAuthZero"
+        class="ml-5"
+      >ログイン</v-btn>
+      <v-btn
+        v-if="this.$auth.loggedIn"
+        type="primary"
+        @click="logoutWithAuthZero"
+        class="ml-5"
+      >ログアウト</v-btn>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    loginWithAuthZero() {
+      this.$auth.loginWith('auth0')
+    },
+    async logoutWithAuthZero() {
+      await this.$auth.logout()
+    }
+  }
+}
+</script>
